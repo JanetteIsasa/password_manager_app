6a0d212f8bf3b489611d4c3dd87a6ed798f6feae
patricia_valdez <pativaldez0@gmail.com>
2022-08-27 15:33:36 -0400

    added color variables

--- lib/app_colors.dart
+++ lib/app_colors.dart
@@ -2,4 +2,11 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static const Color primary = Color(0xff1354b9);
+  static const Color secondary = Color(0xff7AADFC);
+  static const Color inputBackground = Color(0xff7AADFC);
+  static const Color menuBackground = Color(0xff7AADFC);
+  static const Color listBackground = Color(0xff7AADFC);
+  static const Color dangerColor = Color(0xffE64D4E);
+  static const Color inputLabels = Color(0xff8A8A8A);
+  static const Color inputIconColor = Color(0xff8A8A8A);
 }
