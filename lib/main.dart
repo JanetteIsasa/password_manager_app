f68d05d2765d73d32095f5143ae6e8bb9a9a2674
patricia_valdez <pativaldez0@gmail.com>
2022-08-31 14:54:46 -0400

    added new widget in welcome.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:elevenpass/welcome.dart';
 import 'package:flutter/material.dart';
 
 
+
 void main() {
   runApp(const MyApp());
 }
@@ -16,15 +17,13 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
+        textTheme: const TextTheme(
+            headline1: TextStyle(
+            fontSize: 24.0,
+            fontWeight: FontWeight.bold,
+            color: Colors.blueAccent,
+          ),
+        ),
         primarySwatch: Colors.blue,
       ),
       //home: const SplashScreen(),
--- lib/welcome.dart
+++ lib/welcome.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+
+import 'app_colors.dart';
+
+
 class Welcome extends StatelessWidget {
 
   Welcome({Key? key}) : super(key: key);
@@ -7,61 +11,105 @@ class Welcome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:
-        Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Container(
-            width: double.infinity,
-            height: 400,
-            color: Colors.red,
-          ),
-          _buildBullets(),
-          Container(
-            width: double.infinity,
-            height: 120,
-            color: Colors.red,
 
-          ),
-        ],
+      backgroundColor: AppColors.primary,
+      body: SafeArea(
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              contentInfo(),
+              buildBullets(),
+              contentBut(),
+            ],
+        ),
       ),
+
 
     );
   }
 
-  Widget _buildBullets() {
+  Widget contentInfo(){
+    return
+      Container(
+        padding: EdgeInsets.only(left: 20.0, right: 20.0),
+        width: double.infinity,
+        height: 400,
+
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            const Text(
+                "Hello",
+                style: TextStyle(fontSize: 48.0, fontWeight: FontWeight.bold, color: Colors.white) ,
+            ),
+            Image.asset('assets/images/security.png', width: 200,),
+            //Lottie.asset("assets/lottie/security-pay.json"),
+            const Text(
+              "An Internet user has to remember, on average, a total of 10 passwords each day. One in three users are forced to write down their passwords so as not to forget them",
+              style: TextStyle(
+                fontSize: 18.0,
+                color: Colors.white,
+              ),
+            )
+
+          ],
+        ),
+
+    );
+  }
+  Widget buildBullets() {
     return Padding(
       padding: EdgeInsets.all(8),
+      
       child: Row(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Container(
-            width: 8,
-            height: 8,
+            width: 5,
+            height: 5,
             decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(30),
-                color: Colors.red,
+                color: AppColors.menuBackground,
             ),
           ),
           Container(
-            width: 8,
-            height: 8,
+            width: 5,
+            height: 5,
             margin: EdgeInsets.all(8),
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(30),
-              color: Colors.grey,
+              color: Colors.white,
             ),
           ),
           Container(
-            width: 8,
-            height: 8,
+            width: 5,
+            height: 5,
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(30),
-              color: Colors.grey,
+              color: Colors.white,
             ),
           )
         ],
       ),
+    );
+  }
+
+  Widget contentBut(){
+    return Center(
+      child: Container(
+        width: double.infinity,
+        height: 150,
+        color: Colors.white,
+
+        child: const Text(
+          "Login In To Your Account ",
+          style: TextStyle(
+            fontSize: 20,
+          ),
+        ),
+      )
+
+
     );
   }
 }
