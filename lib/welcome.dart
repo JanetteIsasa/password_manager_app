37b3ef756b950542d9d202e2232b2af8ba91fa10
patricia_valdez <pativaldez0@gmail.com>
2022-09-01 19:10:11 -0400

    add lottie in to project

--- lib/welcome.dart
+++ lib/welcome.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-
-
+import 'package:lottie/lottie.dart';
+import 'dart:ui' as ui;
 import 'app_colors.dart';
 
 
@@ -10,6 +10,7 @@ class Welcome extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
     return Scaffold(
 
       backgroundColor: AppColors.primary,
@@ -19,9 +20,18 @@ class Welcome extends StatelessWidget {
             children: [
               contentInfo(),
               buildBullets(),
-              CustomPaint(
-                size: Size(double.infinity,(100*0.5833333333333334).toDouble()),
-                painter: RPSCustomPainter(),
+              Expanded(child:
+                Container(
+                  width: double.infinity,
+
+                  child: CustomPaint(
+                    size: Size(double.infinity, size.height),
+                    painter: RPSCustomPainter(),
+
+                  ),
+
+
+                )
               )
             ],
 
@@ -40,8 +50,7 @@ class Welcome extends StatelessWidget {
       Container(
         padding: EdgeInsets.only(left: 20.0, right: 20.0),
         width: double.infinity,
-        height: 485,
-
+        height: 480,
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
@@ -49,8 +58,9 @@ class Welcome extends StatelessWidget {
                 "Hello",
                 style: TextStyle(fontSize: 48.0, fontWeight: FontWeight.bold, color: Colors.white) ,
             ),
-            Image.asset('assets/images/security.png', width: 200,),
-            //Lottie.asset("assets/lottie/security-pay.json"),
+            //Image.asset('assets/images/security.png', width: 200,),
+            Lottie.asset("assets/lottie/cyber-security.json", height: 250),
+            //Lottie.network("https://assets5.lottiefiles.com/packages/lf20_ndt8zfny.json"),
             const Text(
               "An Internet user has to remember, on average, a total of 10 passwords each day. One in three users are forced to write down their passwords so as not to forget them",
               textAlign: TextAlign.center,
@@ -67,7 +77,7 @@ class Welcome extends StatelessWidget {
   }
   Widget buildBullets() {
     return Container(
-      color: AppColors.primary,
+      color: AppColors.inputIconColor,
       height: 40,
       child: Row(
         mainAxisAlignment: MainAxisAlignment.center,
@@ -102,31 +112,6 @@ class Welcome extends StatelessWidget {
     );
   }
 
-  Widget contentBut(){
-    return Container(
-        width: double.infinity,
-        height: 130,
-        decoration: const BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.only(
-              topRight: Radius.circular(80),
-
-            )
-        ),
-        alignment: Alignment.center,
-        child: const Text(
-            "Login In To Your Account ",
-            style: TextStyle(
-            fontSize: 20,
-          ),
-        ),
-
-
-    );
-
-
-  }
-
 }
 
 class RPSCustomPainter extends CustomPainter{
@@ -134,28 +119,27 @@ class RPSCustomPainter extends CustomPainter{
   @override
   void paint(Canvas canvas, Size size) {
 
-
-
     Paint paint0 = Paint()
       ..color = const Color.fromARGB(255, 255, 255, 255)
       ..style = PaintingStyle.fill
       ..strokeWidth = 1;
 
 
-    Path path0 = Path();
-    path0.moveTo(size.width*0.3333333,size.height*0.3571429);
-    path0.quadraticBezierTo(size.width*0.3327083,size.height*0.4632143,size.width*0.3325000,size.height*0.4985714);
-    path0.lineTo(size.width*0.5433333,size.height*0.5000000);
-    path0.lineTo(size.width*0.5416667,size.height*0.3557143);
-    path0.quadraticBezierTo(size.width*0.5245833,size.height*0.3235714,size.width*0.5141667,size.height*0.3200000);
-    path0.quadraticBezierTo(size.width*0.5031250,size.height*0.3017857,size.width*0.4716667,size.height*0.3028571);
-    path0.lineTo(size.width*0.4050000,size.height*0.3014286);
-    path0.quadraticBezierTo(size.width*0.3706250,size.height*0.3003571,size.width*0.3583333,size.height*0.2857143);
-    path0.quadraticBezierTo(size.width*0.3433333,size.height*0.2782143,size.width*0.3325000,size.height*0.2471429);
-    path0.quadraticBezierTo(size.width*0.3327083,size.height*0.2746429,size.width*0.3333333,size.height*0.3571429);
-    path0.close();
+    Path path = Path();
+    path.moveTo(0, 20); // Start
+    path.quadraticBezierTo(0.5 * 0.20, 0, size.width * 0.35, 0);
+    path.quadraticBezierTo(size.width * 0.40, 0, size.width * 0.40, 20);
+    path.arcToPoint(Offset(size.width * 0.60, 20), radius: Radius.circular(20.0), clockwise: false);
+    path.quadraticBezierTo(size.width * 0.60, 0, size.width * 0.65, 0);
+    path.quadraticBezierTo(size.width * 0.80, 0, size.width, 20);
+    path.lineTo(size.width, size.height);
+    path.lineTo(0, size.height);
+    path.lineTo(0, 20);
+    canvas.drawShadow(path, Colors.black, 5, true);
 
-    canvas.drawPath(path0, paint0);
+    path.close();
+
+    canvas.drawPath(path, paint0);
 
 
   }
