3265ef9ca4ac0867ca30d1d1fd9b6e5ae25c3d3c
patricia_valdez <pativaldez0@gmail.com>
2022-09-07 23:00:10 -0400

    corrections in the welcome

--- lib/welcome.dart
+++ lib/welcome.dart
@@ -26,13 +26,14 @@ class Welcome extends StatelessWidget {
     ),
   );
 
+
   Welcome({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     final Size size = MediaQuery.of(context).size;
-    return Scaffold(
 
+    return Scaffold(
       backgroundColor: AppColors.primary,
       body: SafeArea(
         child: Expanded(
@@ -83,32 +84,42 @@ class Welcome extends StatelessWidget {
   }
 
   Widget contentPage(){
+    const title = Text(
+      "Hello!",
+      style: TextStyle(
+          fontSize: 48.0,
+          fontFamily: 'MontserratAlternates',
+          fontWeight: FontWeight.w600,
+          color: Colors.white) ,
+    );
+    const descripcionText = Text(
+      "An Internet user has to remember, on average, a total of 10 passwords each day. One in three users are forced to write down their passwords so as not to forget them",
+      textAlign: TextAlign.center,
+      style: TextStyle(
+        fontSize: 18.0,
+        color: Colors.white,
+        fontWeight: FontWeight.w500,
+      ),
+    );
+
     return
       Container(
         padding: EdgeInsets.all(25),
         width: double.infinity,
         height: double.infinity,
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            const Text(
-                "Hello!",
-                style: TextStyle(
-                    fontSize: 48.0,
-                    fontFamily: 'MontserratAlternates',
-                    fontWeight: FontWeight.w600,
-                    color: Colors.white) ,
-            ),
-            //Image.asset('assets/images/security.png', width: 200,),
-            Lottie.asset("assets/lottie/cyber-security.json", height: 250),
-            const Text(
-              "An Internet user has to remember, on average, a total of 10 passwords each day. One in three users are forced to write down their passwords so as not to forget them",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 18.0,
-                color: Colors.white,
-                fontWeight: FontWeight.w500,
+            Container(
+              height: 60,
+              child: const Center(
+                child: title,
               ),
+            ),
+            Lottie.asset("assets/lottie/security-pay.json", height: 200),
+            Container(
+              height: 120,
+              child: descripcionText,
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
@@ -125,6 +136,23 @@ class Welcome extends StatelessWidget {
     );
   }
   Widget contentPage2(){
+    const title = Text(
+      "Vaults",
+      style: TextStyle(
+          fontSize: 48.0,
+          fontFamily: 'MontserratAlternates',
+          fontWeight: FontWeight.w600,
+          color: Colors.white) ,
+    );
+    const descripcionText = Text(
+      "With this app you can add all the accounts you want by carrying out an order through vaults, this way it will be much easier to access them.",
+      textAlign: TextAlign.center,
+      style: TextStyle(
+        fontSize: 18.0,
+        color: Colors.white,
+        fontWeight: FontWeight.w500,
+      ),
+    );
     return
       Container(
         padding: EdgeInsets.all(25),
@@ -132,26 +160,18 @@ class Welcome extends StatelessWidget {
         height: double.infinity,
 
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            const Text(
-              " Vaults ",
-              style: TextStyle(
-                  fontSize: 48.0,
-                  fontFamily: 'MontserratAlternates',
-                  fontWeight: FontWeight.w600,
-                  color: Colors.white) ,
-            ),
-            //Image.asset('assets/images/security.png', width: 200,),
-            Lottie.asset("assets/lottie/96833-login.json", height: 250),
-            const Text(
-              "With this app you can add all the accounts you want by carrying out an order through vaults, this way it will be much easier to access them.",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 18.0,
-                color: Colors.white,
-                fontWeight: FontWeight.w500,
+            Container(
+              height: 60,
+              child: const Center(
+                child: title,
               ),
+            ),
+            Lottie.asset("assets/lottie/96833-login.json", height: 250),
+            Container(
+            height: 120,
+            child: descripcionText,
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
@@ -166,34 +186,42 @@ class Welcome extends StatelessWidget {
       );
   }
   Widget contentPage3(){
+    const title = Text(
+      "Password",
+      style: TextStyle(
+          fontSize: 48.0,
+          fontFamily: 'MontserratAlternates',
+          fontWeight: FontWeight.w600,
+          color: Colors.white) ,
+    );
+    const descripcionText = Text(
+      "If you do not remember your password you can check it by entering your trunk and your account. You only have to remember a single password that is that of this application",
+      textAlign: TextAlign.center,
+      style: TextStyle(
+        fontSize: 18.0,
+        color: Colors.white,
+        fontWeight: FontWeight.w500,
+      ),
+    );
     return
       Container(
         padding: EdgeInsets.all(25),
         width: double.infinity,
         height: double.infinity,
-
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            const Text(
-              "Password",
-              style: TextStyle(
-                  fontSize: 48.0,
-                  fontFamily: 'MontserratAlternates',
-                  fontWeight: FontWeight.w600,
-                  color: Colors.white) ,
-            ),
-            Lottie.asset("assets/lottie/forgot-password.json", height: 180),
-            const Text(
-              "If you do not remember your password you can check it by entering your trunk and your account. You only have to remember a single password that is that of this application",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 18.0,
-                color: Colors.white,
-                fontWeight: FontWeight.w500,
+            Container(
+              height: 60,
+              child: const Center(
+                child: title,
               ),
             ),
-
+            Lottie.asset("assets/lottie/forgot-password.json", height: 150),
+            Container(
+              height: 120,
+              child: descripcionText,
+            ),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
