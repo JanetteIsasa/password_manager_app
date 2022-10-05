4c395c19e3d5f40b999898abce960a0b5586a014
patricia_valdez <pativaldez0@gmail.com>
2022-10-05 00:06:34 -0300

    add image and container

--- lib/Accounst_Page/ui/screens/edit_profile.dart
+++ lib/Accounst_Page/ui/screens/edit_profile.dart
@@ -10,35 +10,54 @@ class EditProfile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: AppColors.primary,
-      appBar: AppBar(
-        toolbarHeight: 80,
         backgroundColor: AppColors.primary,
-        elevation: 0,
-        leading: const ArrowButton(),
-        leadingWidth: 75,
-        title: const TextAppBar(
-          "Edit Profile",
-        ),
-        centerTitle: true,
-        actions: const [
-          IconButton(
-              onPressed: null,
-              icon: Icon(
-                Icons.check,
-                size: 30,
-                color: Colors.white,
-              )
-          ),
-          SizedBox(
-            width: 15,
-          ),
-        ]
-      ),
-      body: Container(
-        color: Colors.white,
-        
-      )
-    );
+        appBar: AppBar(
+            toolbarHeight: 80,
+            backgroundColor: AppColors.primary,
+            elevation: 0,
+            leading: const ArrowButton(),
+            leadingWidth: 75,
+            title: const TextAppBar(
+              "Edit Profile",
+            ),
+            centerTitle: true,
+            actions: const [
+              IconButton(
+                  onPressed: null,
+                  icon: Icon(
+                    Icons.check,
+                    size: 30,
+                    color: Colors.white,
+                  )),
+              SizedBox(
+                width: 15,
+              ),
+            ]),
+        body: Column(
+          children: [
+            Container(
+              width: 90,
+              height: 90,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(50.0),
+                  color: Colors.grey),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(50.0),
+                child: Image.asset('assets/images/imageUser.png',
+                    fit: BoxFit.cover),
+              ),
+            ),
+            Expanded(
+                child: Container(
+
+                  decoration: const BoxDecoration(
+                      color: Colors.white,
+                    borderRadius: BorderRadius.vertical(top: Radius.circular(100), )
+                  ),
+
+                )
+            )
+          ],
+        ));
   }
 }
